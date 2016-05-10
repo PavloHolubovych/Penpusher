1ac4cd4fdbfe712201387c497c402b6ff51bd794
markiyan.trychuk <markiyan.trychuk@LV-WS34>
2016-05-10 17:39:32 +0300

    Added lost files

---
+++ Penpusher/Penpusher/Interfaces/IArticleService.cs
@@ -0,0 +1,12 @@
+﻿using Penpusher;
+using System;
+using System.Web;
+
+namespace Penpusher
+{
+    public interface IArticleService
+    {
+        void AddArticle(Article article);
+        bool CheckDoesExists(string title);
+    }
+}
---
+++ Penpusher/Penpusher/Services/ArticleService.cs
@@ -0,0 +1,31 @@
+using Penpusher;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Penpusher
+{
+    class ArticleService : ServiceBase<Article>, IArticleService
+    {
+        private readonly IRepository<Article> repository;
+
+        public ArticleService(IRepository<Article> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void AddArticle(Article article)
+        {
+            repository.Add(article);
+        }
+
+        public bool CheckDoesExists(string title)
+        {
+            return true;
+        }
+
+        public override IEnumerable<Article> Find(string title)
+        {
+            return repository.GetAll<Article>().Where(_ => _.Title == title);
+        }
+    }
+}
