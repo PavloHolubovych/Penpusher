ec4684912e5e8a2ddbc22cabedf85ccb962c674a
Vadym.Khvits <khvits.vadim@gmail.com>
2016-05-10 19:39:12 +0300

    Merged commits from other users

---
+++ Classwork/WebApplication1/Report.Extensibility/IReportInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Report.Extensibility
+{
+    public interface IReportInfo
+    {
+        DateTime TimeStamp { get; }
+
+        string Name { get; }
+
+        int Sum { get; }
+    }
+}
