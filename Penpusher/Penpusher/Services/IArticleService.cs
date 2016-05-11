7edc4cdd3dc8118bbfed06f7dddc722bce3e187b
Pavlo <arexik@gmail.com>
2016-05-11 15:35:48 +0300

    Added missing files

---
+++ Penpusher/Penpusher/Services/IArticleService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using Penpusher.DAL;
+using Penpusher.Services.Base;
+
+namespace Penpusher.Services
+{
+    interface IArticleService
+    {
+    }
+}
