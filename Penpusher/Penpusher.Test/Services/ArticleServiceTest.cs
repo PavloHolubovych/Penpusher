e13c64186bea0ae481c3668de6835c98804a08c7
artem <artem@KV-WS05>
2016-05-12 14:09:02 +0300

    added test for Find()

--- Penpusher/Penpusher.Test/Services/ArticleServiceTest.cs
+++ Penpusher/Penpusher.Test/Services/ArticleServiceTest.cs
@@ -78,5 +78,44 @@
 
             Assert.AreEqual(expected, actual);
         }
+        [TestFixture]
+        public class ArticleFindTest //: TestBase
+        {
+            private Mock<IRepository<Article>> repository;
+
+            [OneTimeSetUp]
+            public void Initialize()
+            {
+                //    base.Initialize();
+                repository = new Mock<IRepository<Article>>();
+            }
+
+            [Test]
+            public void should_find_simple_article_by_title()
+            {
+                //arrange
+                const string testTitle = "asda";
+                var articles = new List<Article>
+            {
+                new Article
+                {
+                    Title = testTitle
+                },
+                new Article
+                {
+                    Title = "qwerty"
+                }
+
+            };
+
+                var target = new ArticleService(repository.Object);
+                repository.Setup(repository => repository.GetAll<Article>())
+                    .Returns(articles);
+                //act
+                var result = target.Find(testTitle);
+                //assert
+                Assert.IsTrue(result.Count() == 1 && result.FirstOrDefault()?.Title == testTitle);
+            }
+        }
     }
 }
