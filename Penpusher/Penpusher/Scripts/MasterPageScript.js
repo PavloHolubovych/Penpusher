771c26e80500efc544146579a8cb80649ca32b10
Maksym Kolomiets <p.s.klom@gmail.com>
2016-05-18 14:00:10 +0300

    change MasterPageView get subscriptions for select

--- Penpusher/Penpusher/Scripts/MasterPageScript.js
+++ Penpusher/Penpusher/Scripts/MasterPageScript.js
@@ -7,8 +7,43 @@
 
         });
     });
-    $("#sideBarSelect").change(function () {
 
+
+    
+    
+    var DrawSubscriptions = function (result) {
+
+        ko.applyBindings({
+            availableCountries: result
+        });
+
+        //var viewModel = {
+        //    // These are the initial options
+        //    availableCountries: ko.observableArray(result)
+        //};
+
+        //// ... then later ...
+        //viewModel.availableCountries.push('China'); // Adds another option
+    };
+
+    
+
+    $.ajax({
+        url: window.location.origin + "/api/Subscriprions/Get",
+        method: "GET",
+        success: function (result) {
+            DrawSubscriptions(result);
+        },
+        error: function (request, textStatus) {
+            alert("Error: " + textStatus);
+        }
+    });
+
+
+
+
+    $("#sideBarSelect").change(function () {
+        
     });
 
 });
