c03f9b5215d3e064dbf2188db8a990eb0eba64d6
Artem.Kvach <akvach85@gmail.com>
2016-05-19 14:38:22 +0300

    52: added Test UnSubscribe

--- Penpusher/Penpusher/Services/INewsProviderService.cs
+++ Penpusher/Penpusher/Services/INewsProviderService.cs
@@ -1,43 +1,18 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="INewsProviderService.cs" company="">
-//   
-// </copyright>
-// <summary>
-//   The NewsProviderService interface.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace Penpusher.Services
+﻿namespace Penpusher.Services
 {
     using System.Collections.Generic;
 
-    /// <summary>
-    /// The NewsProviderService interface.
-    /// </summary>
     public interface INewsProviderService
     {
-        /// <summary>
-        /// The add news provider. 
-        /// </summary>
-        /// <param name="newsProvider">
-        /// The news provider.
-        /// </param>
-        /// <returns>
-        /// The <see cref="NewsProvider"/>.
-        /// </returns>
+        
         IEnumerable<NewsProvider> GetAll();
+
         IEnumerable<NewsProvider> GetByUserId(int id);
 
-
         NewsProvider AddNewsProvider(NewsProvider newsProvider);
+
         UsersNewsProvider AddSubscription(string link);
 
-        /// <summary>
-        /// The delete news provider.
-        /// </summary>
-        /// <param name="id">
-        /// The id.
-        /// </param>
         void DeleteNewsProvider(int id);
     }
 }
