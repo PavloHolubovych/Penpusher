f261fea3aaf6fb4fc218ea8e5f97e05b33b2fe62
MarkMarkiyan <trychuk.mark@gmail.com>
2016-05-20 18:41:21 +0300

    Fixed iframe view

--- Penpusher/Penpusher/Scripts/ArticleDetailsScript.js
+++ Penpusher/Penpusher/Scripts/ArticleDetailsScript.js
@@ -7,12 +7,12 @@
        };
        $.get("/api/Articles/GetArticleDetail?articleId=25",
         function (data) {
-            var article = new ArticleModel(data.Title,  data.Link);
+            var article = new ArticleModel(data.Title, data.Link);
+           
             ko.applyBindings(article, document.getElementById("#articleContent"));
         });
 
        $.get("/api/Articles/MarkAsRead?userId=5&articleId=29",
-        function (data) {
-            var article = new ArticleModel(data.Title, data.Description, data.Link); 
+        function (data) { 
         });
    });
