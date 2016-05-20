3d4113a99c7b2fbd53cb4d53a1fc3d27d039c43e
MarkMarkiyan <trychuk.mark@gmail.com>
2016-05-20 13:04:41 +0300

    Initialize rss parser

---
+++ Penpusher/Penpusher/Services/ContentService/RssReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Penpusher.Services.ContentService
+{
+    public class RssReader
+    {
+        public IEnumerable<string> ReadFeed(string url)
+        {
+            var rssFeed = XDocument.Load(url);
+            Console.Write(rssFeed);
+            var posts = from item in rssFeed.Descendants("item")
+                select item.Element("link").Value;
+
+            return posts;
+        }
+    }
+}
