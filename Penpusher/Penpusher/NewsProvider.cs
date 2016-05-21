94b16d4a362977f670721ba516b027ff6931b2a6
Vadym.Khvits <Vadym.Khvits@WS83>
2016-05-21 14:14:20 +0300

    Added field LastBuildDate to entity class NewsProvider and in database as well.

--- Penpusher/Penpusher/NewsProvider.cs
+++ Penpusher/Penpusher/NewsProvider.cs
@@ -27,7 +27,8 @@ namespace Penpusher
         public string Description { get; set; }
         public string RssImage { get; set; }
         public System.DateTime SubscriptionDate { get; set; }
-    
+        public System.DateTime LastBuildDate { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Article> Articles { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
