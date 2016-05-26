1953a466ac3b95e0451395e023baf6589d893f0f
Vadym.Khvits <Vadym.Khvits@WS83>
2016-05-26 19:37:06 +0300

    78: RssParserTest refactoring finished

--- Penpusher/Penpusher.Test/Services/ContentService/RSSParserTest.cs
+++ Penpusher/Penpusher.Test/Services/ContentService/RSSParserTest.cs
@@ -10,7 +10,7 @@ using Penpusher.Services.ContentService;
 namespace Penpusher.Test.Services.ContentService
 {
     [TestFixture]
-    class RssParserTest : TestBase
+    public class RssParserTest : TestBase
     {
         [SetUp]
         public override void Testinitialize()
@@ -23,24 +23,30 @@ namespace Penpusher.Test.Services.ContentService
         [TestCase(TestName = "Checks correct rss parsing")]
         public void GetParsedArticlesTest()
         {
-            var rssModel = new RssChannelModel()
+            var rssModel = new RssChannelModel
             {
                 RssFile = new XDocument(
-                    new XElement("rss", new XAttribute("version", "2.0"),
-                        new XElement("chanel",
+                    new XElement(
+                        "rss",
+                        new XAttribute("version", "2.0"),
+                        new XElement(
+                            "chanel",
                             new XElement("title", "Title"),
                             new XElement("link", "Link")),
-                        new XElement("item",
+                        new XElement(
+                            "item",
                             new XElement("title", "Test Title1"),
                             new XElement("link", "Test link1"),
                             new XElement("description", "Test Description"),
                             new XElement("pubDate", "Mon, 23 May 2016 00:00:00 -0500")),
-                        new XElement("item",
+                        new XElement(
+                            "item",
                             new XElement("title", "Test Title2"),
                             new XElement("link", "Test link2"),
                             new XElement("description", "Test Description"),
                             new XElement("pubDate", "Mon, 23 May 2016 00:00:00 -0500")),
-                        new XElement("item",
+                        new XElement(
+                            "item",
                             new XElement("title", "Test Title3"),
                             new XElement("link", "Test link3"),
                             new XElement("description", "Test Description"),
@@ -48,14 +54,26 @@ namespace Penpusher.Test.Services.ContentService
                 ProviderId = 1
             };
 
-            var expected = new List<Article>()
+            var expected = new List<Article>
             {
-                new Article() { Date = DateTime.Parse("Mon, 23 May 2016 00:00:00 -0500"), Description = "Test Description", Link = "Test link1", Title = "Test Title1", Id = 0, IdNewsProvider = 1, UsersArticles = null },
-                new Article() { Date = DateTime.Parse("Mon, 23 May 2016 00:00:00 -0500"), Description = "Test Description", Link = "Test link2", Title = "Test Title2", Id = 0, IdNewsProvider = 1, UsersArticles = null  },
-                new Article() { Date = DateTime.Parse("Mon, 23 May 2016 00:00:00 -0500"), Description = "Test Description", Link = "Test link3", Title = "Test Title3", Id = 0, IdNewsProvider = 1, UsersArticles = null  }
+                new Article
+                {
+                    Date = DateTime.Parse("Mon, 23 May 2016 00:00:00 -0500"), Description = "Test Description",
+                    Link = "Test link1", Title = "Test Title1", Id = 0, IdNewsProvider = 1, UsersArticles = null
+                },
+                new Article
+                {
+                    Date = DateTime.Parse("Mon, 23 May 2016 00:00:00 -0500"), Description = "Test Description",
+                    Link = "Test link2", Title = "Test Title2", Id = 0, IdNewsProvider = 1, UsersArticles = null
+                },
+                new Article
+                {
+                    Date = DateTime.Parse("Mon, 23 May 2016 00:00:00 -0500"), Description = "Test Description",
+                    Link = "Test link3", Title = "Test Title3", Id = 0, IdNewsProvider = 1, UsersArticles = null
+                }
             };
 
-            var actual = MockKernel.Get<IParser>().GetParsedArticles(rssModel);
+            IEnumerable<Article> actual = MockKernel.Get<IParser>().GetParsedArticles(rssModel);
 
             Assert.That(actual, Is.EquivalentTo(expected).Using(new PropertiesEqualityComparer<Article>()));
         }
