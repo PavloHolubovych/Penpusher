e002414b25550e657e1cff43e6e703202232ddb1
Vadym.Khvits <Vadym.Khvits@WS83>
2016-05-26 21:22:53 +0300

    78: NinjectTest refactoring finished.

---
+++ Penpusher/Penpusher.Test/NinjectTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using Ninject;
+using NUnit.Framework;
+using Penpusher.Services.ContentService;
+
+namespace Penpusher.Test
+{
+    [TestFixture]
+    public class NinjectTest
+    {
+        public static IEnumerable<object[]> ControllerTypes
+        {
+            get
+            {
+                return typeof(Startup).Assembly.GetTypes()
+                    .Where(t => typeof(ApiController).IsAssignableFrom(t) || typeof(Controller).IsAssignableFrom(t) || typeof(ProviderTrackingService).IsAssignableFrom(t))
+                    .Select(t => new[] { t });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(ControllerTypes))]
+        public void AllControllersShouldBeCreatable(Type contollerType)
+        {
+            IKernel kernel = NinjectWebCommon.Kernel;
+            Assert.NotNull(kernel.Get(contollerType));
+        }
+    }
+}
