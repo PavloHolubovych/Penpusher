fbb2387473c6e729788704f48adcd14b3132e721
Maksym Kolomiets <p.s.klom@gmail.com>
2016-06-01 12:07:10 +0300

    change blocks visibility

--- Penpusher/Penpusher/Scripts/ArticlePartialViewScript.js
+++ Penpusher/Penpusher/Scripts/ArticlePartialViewScript.js
@@ -1,5 +1,9 @@
 ﻿addToReadLater = function (articleId) {
-    $.post("/api/Articles/ToReadLater", { "articleId": articleId, "flag": true })
+    var element = $(event.target);
+    $.post("/api/Articles/ToReadLater", { "articleId": articleId, "flag": true }).done(function (data) {
+            element.text("Already in read later list");
+            element.addClass("disabled");
+        })
         .error(function (request, textStatus) {
             alert("Error: " + textStatus);
         });
